c79f46a69ea8ce63ce937aa6137b8d4b7ee03804
theeramat1123 <theeramat1123.ts@gmail.com>
2018-11-04 00:15:43 +0700

    Login,Register,Forgot

--- src/pages/ForgotPage.js
+++ src/pages/ForgotPage.js
@@ -1,15 +1,93 @@
 import React from 'react'
-import {View , Text } from 'react-native'
+import {View , Text,StyleSheet,Button,TextInput,TouchableOpacity,ImageBackground,KeyboardAvoidingView,Image } from 'react-native'
+import { Space  ,Font} from '../styles/global';
+import {vw, vh, vmin, vmax} from 'react-native-viewport-units';
 class ForgotPage extends React.Component{
     render(){
         return(
-            <View>
-                <Text>
-                    This is ForgotPage
-                </Text>
+            
+            <ImageBackground source={require('../../assets/imgs/dpho4.png' )} style={styles.background}>
+            <View style={{backgroundColor:'rgba(128,0,128,0.8)',height:'100%',width:'100%'}}>
+            <KeyboardAvoidingView behavior="position" enabled>
+            <View style={{display : "flex" , alignItems : "center" ,}}>
+            <TouchableOpacity style={{marginRight:'auto',marginLeft:5*vw}}>
+                                <View style={{backgroundColor:'rgba(255,255,255,0.26)',height:10*vw,width:15*vw,marginTop:8*vw,
+                                justifyContent:'center',borderRadius:23}}>
+                                    <Image source={require('../../assets/icon/leftarrow.png' ) }  
+                                        style={{height:5*vw,width:5*vw,justifyContent:'center'
+                                        ,marginLeft:'auto',marginRight:'auto'}}
+                                    /> 
+                                </View>
+                            </TouchableOpacity>
+                <Text style={{fontSize : 50,color:'white',marginTop:'30%'}}>Register</Text>
+                
+                <TextInput
+                    underlineColorAndroid='rgba(255,255,255,1)'
+                    style={{height: 10*vw,width:'70%',marginTop:'10%',color:'white'}}
+                    placeholder="Full Name"
+                    onChangeText={(text) => this.setState({text})}
+                />
+                
+                <View style={{flex: 1, flexDirection: 'row',marginLeft:'auto',marginTop:'4%',marginRight:'auto'}}>
+                    <TextInput
+                        underlineColorAndroid='rgba(255,255,255,1)'
+                        style={{height: 10*vw,width:'47%',color:'white',}}
+                        placeholder="Your code of KMITL"
+                        onChangeText={(text) => this.setState({text})}
+                    />
+
+                    <Text style={{color:'white',fontSize:4*vw,marginTop:2*vw,marginBottom:-7*vw}}>@kmitl.ac.th</Text>
+                </View>
+
+                <TextInput
+                    underlineColorAndroid='rgba(255,255,255,1)'
+                    style={{height: 10*vw,width:'70%',marginTop:'10%',color:'white'}}
+                    placeholder="Password"
+                    onChangeText={(text) => this.setState({text})}
+                />
+
+                <TextInput
+                    underlineColorAndroid='rgba(255,255,255,1)'
+                    style={{height: 10*vw,width:'70%',marginTop:'5%',color:'white'}}
+                    placeholder="Re-Password"
+                    onChangeText={(text) => this.setState({text})}
+                />
+
+                <TouchableOpacity style={{backgroundColor:'white',width:'70%',height:'6%',alignItems:'center',borderRadius:14,marginTop:'5%',marginBottom:5*vw}}>
+                    <Text style={{color:'black',fontWeight:'bold',fontSize:4*vw,marginTop:1*vw}}>
+                        Register
+                    </Text>
+                </TouchableOpacity>
+
+                {/* <Button
+                    // onPress={onPressLearnMore}
+                    title="Sign in"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                /> */}
+                
             </View>
+            </KeyboardAvoidingView>
+                </View>
+        </ImageBackground>
+        
         )
     }
 }
+const styles = StyleSheet.create({
+    container : {
+        padding : Space.paddingSize
+         
+        
+     },
+     background :{
+         height:'120%',
+         width:'100%',
+     },
+     Textwhite : {
+ 
+     }
+     
+ })
 
 export default ForgotPage
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,15 +1,79 @@
 import React from 'react'
-import {View , Text } from 'react-native'
+import {View , Text,StyleSheet,Button,TextInput,TouchableOpacity,ImageBackground,KeyboardAvoidingView} from 'react-native'
+import { Space  ,Font} from '../styles/global';
+import {vw, vh, vmin, vmax} from 'react-native-viewport-units';
 class LoginPage extends React.Component{
     render(){
         return(
-            <View>
-                <Text>
-                    This is LoginPage
+        
+        // <ImageBackground source={require('../../assets/imgs/dpho4.png' )} style={styles.background}>
+            <View  style={styles.background}>
+            <KeyboardAvoidingView behavior="position" enabled>
+            <View style={{ alignItems : "center" ,}}>  
+                <Text style={{fontSize : 13*vw,color:'white',marginTop:'30%'}}>Welcom to{"\n"}   Rendez</Text>
+                <Text style={{fontSize : 3.7*vw,color:'white'}}>The best way to do activity with anyone{"\n"} 
+                    and discover new friends.{"\n"}
+                    Let's get start!
                 </Text>
+
+                <TextInput
+                    underlineColorAndroid='rgba(255,255,255,1)'
+                    style={{height: 10*vw,width:'70%',marginTop:'10%',color:'white'}}
+                    placeholder="Email"
+                    onChangeText={(text) => this.setState({text})}
+                />
+
+                <TextInput
+                underlineColorAndroid='rgba(255,255,255,1)'
+                    style={{height: 10*vw,width:'70%',}}
+                    placeholder="Password"
+                    onChangeText={(text) => this.setState({text})}
+                />
+
+                <TouchableOpacity style={{backgroundColor:'white',width:'70%',height:'6%',alignItems:'center',borderRadius:14,marginTop:'5%'}}>
+                    <Text style={{color:'black',fontWeight:'bold',fontSize:4*vw}}>
+                        Sign in
+                    </Text>
+                </TouchableOpacity>
+
+                {/* <Button
+                    // onPress={onPressLearnMore}
+                    title="Sign in"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                /> */}
+                
             </View>
+            </KeyboardAvoidingView>
+            <View style={{flex: 1, flexDirection: 'row',marginLeft:'auto',marginRight:'auto'}}>
+                <TouchableOpacity>
+                    <Text  style={{color:'white',textDecorationLine: 'underline',marginTop:'auto',marginBottom:'20%'}}>Forgot Password?</Text>
+                </TouchableOpacity>
+                     <Text style={{marginLeft:'5%'}}></Text>  
+                <TouchableOpacity>
+                    <Text style={{color:'white',textDecorationLine: 'underline',marginTop:'auto',marginBottom:'40%'}}>Register?</Text>
+                </TouchableOpacity>
+                </View>
+                </View>
+        // </ImageBackground>
+        
         )
     }
 }
+const styles = StyleSheet.create({
+    container : {
+       padding : Space.paddingSize
+        
+       
+    },
+    background :{
+        backgroundColor:'purple',
+        height:'100%',
+        width:'100%',
+    },
+    Textwhite : {
 
+    }
+    
+})
 export default LoginPage
