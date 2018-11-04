e4b51a9e27fd762fedc4c24c6cf5e3c5c9dfad1b
theeramat1123 <theeramat1123.ts@gmail.com>
2018-11-04 21:02:35 +0700

    3 pages

--- src/pages/ForgotPage.js
+++ src/pages/ForgotPage.js
@@ -2,15 +2,16 @@ import React from 'react'
 import {View , Text,StyleSheet,Button,TextInput,TouchableOpacity,ImageBackground,KeyboardAvoidingView,Image } from 'react-native'
 import { Space  ,Font} from '../styles/global';
 import {vw, vh, vmin, vmax} from 'react-native-viewport-units';
+import { LinearGradient } from 'expo';
 class ForgotPage extends React.Component{
     render(){
         return(
             
             <ImageBackground source={require('../../assets/imgs/dpho4.png' )} style={styles.background}>
-            <View style={{backgroundColor:'rgba(128,0,128,0.8)',height:'100%',width:'100%'}}>
-            <KeyboardAvoidingView behavior="position" enabled>
-            <View style={{display : "flex" , alignItems : "center" ,}}>
-            <TouchableOpacity style={{marginRight:'auto',marginLeft:5*vw}}>
+                <View style={{backgroundColor:'rgba(128,0,128,0.8)',height:'100%',width:'100%'}}>
+                    <KeyboardAvoidingView behavior="position" enabled>
+                        <View style={{display : "flex" , alignItems : "center" ,}}>
+                            <TouchableOpacity style={{marginRight:'auto',marginLeft:5*vw}}>
                                 <View style={{backgroundColor:'rgba(255,255,255,0.26)',height:10*vw,width:15*vw,marginTop:8*vw,
                                 justifyContent:'center',borderRadius:23}}>
                                     <Image source={require('../../assets/icon/leftarrow.png' ) }  
@@ -19,57 +20,37 @@ class ForgotPage extends React.Component{
                                     /> 
                                 </View>
                             </TouchableOpacity>
-                <Text style={{fontSize : 50,color:'white',marginTop:'30%'}}>Register</Text>
-                
-                <TextInput
-                    underlineColorAndroid='rgba(255,255,255,1)'
-                    style={{height: 10*vw,width:'70%',marginTop:'10%',color:'white'}}
-                    placeholder="Full Name"
-                    onChangeText={(text) => this.setState({text})}
-                />
-                
-                <View style={{flex: 1, flexDirection: 'row',marginLeft:'auto',marginTop:'4%',marginRight:'auto'}}>
-                    <TextInput
-                        underlineColorAndroid='rgba(255,255,255,1)'
-                        style={{height: 10*vw,width:'47%',color:'white',}}
-                        placeholder="Your code of KMITL"
-                        onChangeText={(text) => this.setState({text})}
-                    />
+                            <Text style={{fontSize : 7.5*vw,color:'white',marginTop:'15%',fontWeight:'bold'}}>Forgot Password?</Text>
 
-                    <Text style={{color:'white',fontSize:4*vw,marginTop:2*vw,marginBottom:-7*vw}}>@kmitl.ac.th</Text>
+                            <LinearGradient
+                                colors={['#F6CECE', '#FB9696', '#FF6060']}
+                                style={{alignItems:'center',borderRadius:5,marginTop:40*vw,position:'relative'}}>
+                                <Text style={{fontSize:4.5*vw,fontWeight:'bold',marginTop:'10%',}}>
+                                    Did you forget your password?
+                                </Text>
+                                <Text style={{fontSize:2.5*vw,marginTop:'2%'}}>
+                                    Enter your email address you're using for your account below{"\n"}
+                                    and we will send you a password reset link.
+                                </Text>
+
+                                <TextInput
+                                    underlineColorAndroid='rgba(0,0,0,0)'
+                                     style={{marginLeft:'5%',marginRight:'5%',height: 6*vw,width:80*vw,marginTop:'5%',color:'black',backgroundColor:'white',borderRadius:50,}}
+                                     placeholder="Enter your email address"
+                                    onChangeText={(text) => this.setState({text})}
+                                />
+
+                                <TouchableOpacity style={{backgroundColor:'white',width:80*vw,height:6*vw,alignItems:'center',borderRadius:14,marginTop:'5%',marginBottom:5*vw}}>
+                                    <Text style={{color:'black',fontWeight:'bold',fontSize:3*vw,marginTop:1*vw}}>
+                                    Request reset link
+                                    </Text>
+                                </TouchableOpacity>
+                                </LinearGradient>
+                                <ImageBackground source={require('../../assets/imgs/Group96.png' )} style={{height:40*vw,width:40*vw,position:'absolute',marginTop:'50%'}}></ImageBackground> 
+                        </View>
+                    </KeyboardAvoidingView>
                 </View>
-
-                <TextInput
-                    underlineColorAndroid='rgba(255,255,255,1)'
-                    style={{height: 10*vw,width:'70%',marginTop:'10%',color:'white'}}
-                    placeholder="Password"
-                    onChangeText={(text) => this.setState({text})}
-                />
-
-                <TextInput
-                    underlineColorAndroid='rgba(255,255,255,1)'
-                    style={{height: 10*vw,width:'70%',marginTop:'5%',color:'white'}}
-                    placeholder="Re-Password"
-                    onChangeText={(text) => this.setState({text})}
-                />
-
-                <TouchableOpacity style={{backgroundColor:'white',width:'70%',height:'6%',alignItems:'center',borderRadius:14,marginTop:'5%',marginBottom:5*vw}}>
-                    <Text style={{color:'black',fontWeight:'bold',fontSize:4*vw,marginTop:1*vw}}>
-                        Register
-                    </Text>
-                </TouchableOpacity>
-
-                {/* <Button
-                    // onPress={onPressLearnMore}
-                    title="Sign in"
-                    color="#841584"
-                    accessibilityLabel="Learn more about this purple button"
-                /> */}
-                
-            </View>
-            </KeyboardAvoidingView>
-                </View>
-        </ImageBackground>
+            </ImageBackground>
         
         )
     }
